cc6c65b2039229ee3c38d4bf8855607cf4febcb4
littleweeden <shelokumacub7321@yahoo.com>
2019-02-01 15:06:05 -0500

    Creating ConnectFour Project

---
+++ ConnectFour/ConnectFour/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void StartGame()
+        {
+            Player player = new Player();
+            Player playerTwo = new Player();
+        }
+
+        protected void NewGame()
+        {
+
+        }
+
+        protected void ResetGame()
+        {
+
+        }
+        
+    }
+}
---
+++ ConnectFour/ConnectFour/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour
+{
+    class Player : Game
+    {
+        public string color;
+        public int turnNumber;
+
+        public string PropPlayer
+        {
+            get
+            {
+                return color;
+            }
+
+            set
+            {
+                color = value;
+            }
+        }
+        
+    }
+}
