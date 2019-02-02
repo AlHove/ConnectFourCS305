ec19034a96d577d2960751eb1e4b501aebf89d22
AlHove <43249047+AlHove@users.noreply.github.com>
2019-02-01 22:23:02 -0500

    Update Board.cs
    Wrote some preliminary stuff. I will fix in afternoon

--- ConnectFour/ConnectFour/Board.cs
+++ ConnectFour/ConnectFour/Board.cs
@@ -1,29 +1,50 @@
-﻿using System;
+// Started by Alyssa Hove
+// 2/1/19
+// Board class: deals with validating spaces and checking for wins and losses
+using System;
 
 public class Board
 {
-    // Intstance Variables 
+    // Instance Variables 
     public int width;
     public int height;
+    public char[,] board;
+   
 
+    // Setter
+    public void SetBoard(int wid, int height)
+    {
+    }
+    
+    // Getter
+    public char[,] GetBoard ()
+    {
+        
+    }
+    
     // methods
-    public bool ValidateLocation ( int row, int col)
+    public bool ValidateLocation ( int row, int col) // validate that location is free
     {
 
     }
 
-    public bool CheckHorWin (int row, int col, token t, Board b){
+    public bool CheckHorWin (int row, int col, token t, Board b) // check if can win horizontally check left until nothing on left then start from beginning and go right
+    {
 
-     }
-    public bool CheckVertWin (int row, int col, token t, Board b)
+    }
+    public bool CheckVertWin (int row, int col, token t, Board b) // check if can win vertically
     {
 
     }
 
-    public bool CheckDiagonalWin(int row, int col, token t, Board b)
+    public bool CheckDiagonalWin(int row, int col, token t, Board b) // check if can win diagonally 
     {
 
+    }
 
+    public void DisplayBoard(int row, int col)
+    {
+        
     }
 
 
