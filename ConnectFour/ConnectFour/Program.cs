568fab90cf3515bfd5a1622d0f94c09aed3edf6f
AlHove <43249047+AlHove@users.noreply.github.com>
2019-02-04 21:24:38 -0500

    Create Program.cs

---
+++ ConnectFour/ConnectFour/Program.cs
@@ -0,0 +1,21 @@
+/*
+Alyssa Hove and Katheryn Weeden
+Main for Connect Four
+Created 2/4/19
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
