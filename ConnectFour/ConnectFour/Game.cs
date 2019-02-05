65195b94a7b82d693c01cf0d78e2f0a82ca40c1e
AlHove <43249047+AlHove@users.noreply.github.com>
2019-02-04 21:27:18 -0500

    Update Game.cs
    Comments

--- ConnectFour/ConnectFour/Game.cs
+++ ConnectFour/ConnectFour/Game.cs
@@ -1,4 +1,9 @@
-﻿using System;
+//Katheryn Weeden and Alyssa Hove
+//Started: 2/1/19
+// Game class
+//Deals with Starting the Game, Stopping the game, and restarting game
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,18 +17,18 @@ namespace ConnectFour
         {
 
         }
-
+// Start the game
         public void StartGame()
         {
             Player player = new Player();
             Player playerTwo = new Player();
         }
-
+// Start a New Game
         protected void NewGame()
         {
 
         }
-
+// Reset the current Game
         protected void ResetGame()
         {
 
