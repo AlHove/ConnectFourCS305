40d531821dccd13a1d67fc4f555d1913b14f7c94
AlHove <43249047+AlHove@users.noreply.github.com>
2019-02-13 16:54:35 -0500

    Update Program.cs

--- ConnectFour/ConnectFour/Program.cs
+++ ConnectFour/ConnectFour/Program.cs
@@ -7,18 +7,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
 using static ConnectFour.Game;
 
 namespace ConnectFour
 {
     class Program
     {
-      
+        //path to saved serialized data
+        const string saveFileName = "Connect4Save.xml"; 
         static void Main(string[] args)
         {
             Game g = new Game();
             int result;
-            Console.WriteLine("Welcome to Connect Four. Would you like to start a new game or continue an old one? Press 0 for new" );
+            if (File.Exists(saveFileName))
+            {
+                Console.WriteLine("Welcome to Connect Four. Would you like to start a new game or continue an old one? Press 0 for new" );
+                if (int.TryParse(Console.ReadLine(), out input))
+                    if (input == 1)
+                        {
+                        Stream saveFile = File.OpenRead(saveFileName);
+                        SoapFormatter deserializer = new SoapFormatter();
+                        g = (Game)(deserializer.Deserialize(saveFile));
+                        saveFile.Close();
+                    }
+                //remove file regardless of restore or not since a new game would be started.
+                File.Delete(saveFileName);
+            }
+            //game not restored so start a new one
+            if (g == null)
+            {
+                g.NewGame();
+            }
             if (int.TryParse(Console.ReadLine(), out result))
             {
                 if (result == 0)
@@ -38,5 +59,7 @@ namespace ConnectFour
         }
     }
 }
+
+
 
 
